1211d5f918953e7c6d0889b02e7659d1ea7260c2
uk <ukiiing00@gmail.com>
2023-06-08 16:18:44 +0900

    PAINTING BOARD: Painting Lines

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
+ctx.lineWidth = 2;
 
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.fillRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(324, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
+const colors = [
+    '#ff3838',
+    '#ffb8b8',
+    '#c56cf0',
+    '#ff9f1a',
+    '#fff200',
+    '#32ff7e',
+    '#7efff5',
+    '#18dcff',
+    '#7d5fff',
+];
 
-ctx.fillRect(210 - 40, 200 - 30, 15, 100);
-ctx.fillRect(350 - 40, 200 - 30, 15, 100);
-ctx.fillRect(260 - 40, 200 - 30, 60, 200);
+function onClick(event) {
+    ctx.beginPath();
+    ctx.moveTo(800, 800);
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.fillStyle = 'white';
-ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+canvas.addEventListener('mousemove', onClick);
